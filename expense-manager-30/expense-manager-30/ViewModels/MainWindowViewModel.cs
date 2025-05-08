c558020fc9611613c7432e943583879b1d957c4b
Martin Cimbal <xcimbal@fi.muni.cz>
2025-05-08 09:59:49 +0200

    project init

---
+++ expense-manager-30/expense-manager-30/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace expense_manager_30.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ expense-manager-30/expense-manager-30/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace expense_manager_30.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
